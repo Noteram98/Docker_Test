1c20b0090948cfb5f3f0d2091ee127dd4cb466b5
blamesx <129559891+blamesx@users.noreply.github.com>
2024-04-19 15:38:21 +0200

    programma.py

--- script/programma.py
+++ script/programma.py
@@ -2,10 +2,16 @@ import datetime
 
 date = datetime.datetime.now()
 
-print(f"Ciao dal container gruppo 2, per noi oggi è{date}")
+print(f"Ciao dal container gruppo 2, per noi oggi è {date}")
+
+print("Vuoi sapere il tuo segno zodiacale cinese? ")
 segno = ["Scimmia","Gallo","Cane","Maiale","Topo","Bue","Tigre"\
 ,"Coniglio","Drago","Serpente","Cavallo","Capra"]
 
-i = int(input("Inserisci un anno:\n"))
+i = int(input("Inserisci il tuo anno di nascita:\n"))
 n = i%12
-print(F"Sei del {segno[n]}\n")
+print(F"Sei del {segno[n]}!\n")
+
+n2 = segno[2024%12]
+
+print(f"Invece chi nasce quest'anno sarà del {n2}")
