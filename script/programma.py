84acd02e6d86a55927844997e90b4848ce82ee7a
cschiavo <105165417+celsoita@users.noreply.github.com>
2024-04-19 15:14:23 +0200

    Update programma.py

--- script/programma.py
+++ script/programma.py
@@ -3,3 +3,9 @@ import datetime
 date = datetime.datetime.now()
 
 print(f"Ciao dal container gruppo 2, per noi oggi è{date}")
+segno = ["Scimmia","Gallo","Cane","Maiale","Topo","Bue","Tigre"\
+,"Coniglio","Drago","Serpente","Cavallo","Capra"]
+
+i = int(input("Inserisci un anno:\n"))
+n = i%12
+print(F"Sei del {segno[n]}\n")
